a9e8143dc9a73431feb8dfcd0993f4ffacafd136
godynovden <godynov.den@gmail.com>
2026-04-18 20:46:18 +0200

    feat(T-02): rename app to Matrix, applicationId rs.ecoinfra.matrix

--- plugins/src/main/kotlin/config/BuildTimeConfig.kt
+++ plugins/src/main/kotlin/config/BuildTimeConfig.kt
@@ -1,16 +1,14 @@
 /*
- * Copyright (c) 2025 Element Creations Ltd.
- * Copyright 2025 New Vector Ltd.
+ * Copyright (c) 2025 Matrix Messenger.
  *
- * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
- * Please see LICENSE files in the repository root for full details.
+ * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 package config
 
 object BuildTimeConfig {
-    const val APPLICATION_ID = "io.element.android.x"
-    const val APPLICATION_NAME = "Element X"
+    const val APPLICATION_ID = "rs.ecoinfra.matrix"
+    const val APPLICATION_NAME = "Matrix"
     const val GOOGLE_APP_ID_RELEASE = "1:912726360885:android:d097de99a4c23d2700427c"
     const val GOOGLE_APP_ID_DEBUG = "1:912726360885:android:def0a4e454042e9b00427c"
     const val GOOGLE_APP_ID_NIGHTLY = "1:912726360885:android:e17435e0beb0303000427c"
