8834c68d49a069d0f6f20adf7b48757e6f2f306b
godynovden <godynov.den@gmail.com>
2026-04-19 21:01:45 +0200

    feat(T-22): set Firebase app ID for matrix-ecoinfra project

--- plugins/src/main/kotlin/config/BuildTimeConfig.kt
+++ plugins/src/main/kotlin/config/BuildTimeConfig.kt
@@ -9,9 +9,9 @@ package config
 object BuildTimeConfig {
     const val APPLICATION_ID = "rs.ecoinfra.matrix"
     const val APPLICATION_NAME = "Matrix"
-    const val GOOGLE_APP_ID_RELEASE = "1:912726360885:android:d097de99a4c23d2700427c"
-    const val GOOGLE_APP_ID_DEBUG = "1:912726360885:android:def0a4e454042e9b00427c"
-    const val GOOGLE_APP_ID_NIGHTLY = "1:912726360885:android:e17435e0beb0303000427c"
+    const val GOOGLE_APP_ID_RELEASE = "1:12830939977:android:c2ec31e96186bc39d45898"
+    const val GOOGLE_APP_ID_DEBUG = "1:12830939977:android:c2ec31e96186bc39d45898"
+    const val GOOGLE_APP_ID_NIGHTLY = "1:12830939977:android:c2ec31e96186bc39d45898"
 
     val METADATA_HOST_REVERSED: String? = null
     val URL_WEBSITE: String? = null
